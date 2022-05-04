e9d8c99569337743ef415d804f41b2e24e43dfb1
Oleksii Verbytskyi <alverba88@gmail.com>
2022-05-04 20:36:14 +0300

    message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      goit-react-hw-01-components
-    </div>
-  );
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  feedbackHandler = event => {
+    const key = event.target.key;
+    this.setState(prevState => ({ [key]: prevState[key] + 1 }));
+  };
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const result = good + neutral + bad;
+    return result;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const allGoals = this.countTotalFeedback();
+    const positiveFeedback = Math.round((good * 100) / allGoals);
+    return positiveFeedback;
+  };
+
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+App.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  feedbackHandler: PropTypes.func,
+  countTotalFeedback: PropTypes.func,
+  countPositiveFeedbackPercentage: PropTypes.func,
 };
