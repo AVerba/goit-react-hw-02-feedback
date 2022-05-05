7b729fa332eb8ba0e60d4b2ae0776f8638086fb2
Oleksii Verbytskyi <alverba88@gmail.com>
2022-05-05 11:31:43 +0300

    Added FeedbackOptions component

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, feedbackHandler }) => {
+  return (
+    <ul className={styles.list}>
+      {options.map((option, index) => (
+        <li className={styles.item} key={index}>
+          <button name={option} type="button" onClick={feedbackHandler}>
+            {option}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  feedbackHandler: PropTypes.func.isRequired,
+};
